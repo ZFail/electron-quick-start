4fef64aa1e59a02a6edf95bcd82f0a693fe7e1ec
ZFail <zfail0@gmail.com>
2023-09-30 00:00:34 +0300

    transparent

--- renderer.js
+++ renderer.js
@@ -1,7 +1,19 @@
-/**
- * This file is loaded via the <script> tag in the index.html file and will
- * be executed in the renderer process for that window. No Node.js APIs are
- * available in this process because `nodeIntegration` is turned off and
- * `contextIsolation` is turned on. Use the contextBridge API in `preload.js`
- * to expose Node.js functionality from the main process.
- */
+const { BrowserWindow } = require("@electron/remote");
+
+// Close app
+document.getElementById("close").addEventListener(
+  "click",
+  () => {
+    BrowserWindow.getFocusedWindow().close();
+  },
+  false
+);
+
+// Close app
+document.getElementById("devtools").addEventListener(
+  "click",
+  () => {
+    BrowserWindow.getFocusedWindow().openDevTools();
+  },
+  false
+);
